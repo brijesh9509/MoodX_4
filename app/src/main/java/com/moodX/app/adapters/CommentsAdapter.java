b079eac74ca9e1916e2283c62e4e9b4f57245776
brijesh9509 <jsk143fams@gmail.com>
2023-02-25 17:59:42 +0530

    vCode 7

--- app/src/main/java/com/moodX/app/adapters/CommentsAdapter.java
+++ app/src/main/java/com/moodX/app/adapters/CommentsAdapter.java
@@ -2,7 +2,10 @@ package com.moodX.app.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,28 +17,24 @@ import com.mikhaellopez.circularimageview.CircularImageView;
 import com.moodX.app.models.GetCommentsModel;
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.OriginalViewHolder> {
 
-    private List<GetCommentsModel> items = new ArrayList<>();
+    private List<GetCommentsModel> items;
     private Context ctx;
-
-
 
     public CommentsAdapter(Context context, List<GetCommentsModel> items) {
         this.items = items;
         ctx = context;
-
     }
 
-
+    @NonNull
     @Override
     public CommentsAdapter.OriginalViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         CommentsAdapter.OriginalViewHolder vh;
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_comment, parent, false);
-        vh = new CommentsAdapter.OriginalViewHolder(v);
+        vh = new OriginalViewHolder(v);
         return vh;
     }
 
@@ -44,22 +43,16 @@ public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.Origin
 
         final GetCommentsModel obj = items.get(position);
 
-
         holder.name.setText(obj.getUserName());
         holder.comment.setText(obj.getComments());
 
         Picasso.get().load(obj.getUserImgUrl()).placeholder(R.drawable.ic_avatar).into(holder.imageView);
 
-
-        holder.reply.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent=new Intent(ctx, ReplyActivity.class);
-                intent.putExtra("commentId",obj.getCommentsId());
-                intent.putExtra("videoId",obj.getVideosId());
-                ctx.startActivity(intent);
-            }
+        holder.reply.setOnClickListener(v -> {
+            Intent intent = new Intent(ctx, ReplyActivity.class);
+            intent.putExtra("commentId", obj.getCommentsId());
+            intent.putExtra("videoId", obj.getVideosId());
+            ctx.startActivity(intent);
         });
     }
 
@@ -68,20 +61,16 @@ public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.Origin
         return items.size();
     }
 
-    public class OriginalViewHolder extends RecyclerView.ViewHolder {
-
-        private TextView name,comment,reply;
-        private View lyt_parent;
+    public static class OriginalViewHolder extends RecyclerView.ViewHolder {
+        private TextView name, comment, reply;
         private CircularImageView imageView;
-
 
         public OriginalViewHolder(View v) {
             super(v);
             name = v.findViewById(R.id.name);
-            lyt_parent=v.findViewById(R.id.lyt_parent);
-            imageView=v.findViewById(R.id.profile_img);
-            comment=v.findViewById(R.id.comments);
-            reply=v.findViewById(R.id.tv_replay);
+            imageView = v.findViewById(R.id.profile_img);
+            comment = v.findViewById(R.id.comments);
+            reply = v.findViewById(R.id.tv_replay);
         }
     }
 
